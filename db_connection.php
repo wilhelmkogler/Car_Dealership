a860884274e9c0ec5155e4574593565173af8ba2
Wilhelm Kogler <99600079+wilhelmkogler@users.noreply.github.com>
2025-03-26 16:25:31 +0100

    Update db_connection.php

--- db_connection.php
+++ db_connection.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "kwheels_english";
+$dbname = "kwheels";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
